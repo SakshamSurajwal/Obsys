a5854779775589a13fb131b27b4aa7c53f3fcab4
Saksham Surajwal <sakshamsurajwal177@gmail.com>
2026-05-30 13:53:39 +0530

    channel added and pushing all changes

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -2,17 +2,22 @@ package api
 
 import (
 	"Obsys/internal/metrices"
+	"Obsys/internal/service"
 	"encoding/json"
 	"net/http"
-	"time"
 )
 
 type Handler struct {
-	storage *metrices.Storage
+	metricChan chan metrices.Metric
+	queryChan  chan (chan []metrices.Metric)
+	service    *service.MetricService
 }
 
-func NewHandler(storage *metrices.Storage) *Handler {
-	return &Handler{storage}
+func NewHandler(metricChan chan metrices.Metric, service *service.MetricService) *Handler {
+	return &Handler{
+		metricChan: metricChan,
+		service:    service,
+	}
 }
 
 func (h *Handler) SaveMetric(res http.ResponseWriter, req *http.Request) {
@@ -23,17 +28,18 @@ func (h *Handler) SaveMetric(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	metric.Timestamp = time.Now()
-
-	h.storage.SaveMetric(metric)
+	h.metricChan <- metric
 
 	res.WriteHeader(http.StatusCreated)
 
-	json.NewEncoder(res).Encode(map[string]string{"message": "Metric created successfullly"})
+	json.NewEncoder(res).Encode(map[string]string{"message": "Metric created successfully"})
 }
 
 func (h *Handler) GetMetrics(res http.ResponseWriter, req *http.Request) {
-	var metric = h.storage.GetMetrics()
+	var responseChan = make(chan []metrices.Metric)
+
+	h.queryChan <- responseChan
+	var metric = <-responseChan
 
 	res.Header().Set("Content-type", "application/json")
 
--- internal/service/worker.go
+++ internal/service/worker.go
@@ -1,9 +1,14 @@
-package metrices
+package service
 
-import "Obsys/internal/service"
+import "Obsys/internal/metrices"
 
-func SaveWorker(metricChan chan Metric, service *service.MetricService) {
-	for metric := range metricChan {
-		service.SaveMetric(metric)
+func SaveWorker(metricChan chan metrices.Metric, queryChan chan (chan []metrices.Metric), service *MetricService) {
+	for {
+		select {
+		case metric := <-metricChan:
+			service.SaveMetric(metric)
+		case query := <-queryChan:
+			query <- service.GetMetric()
+		}
 	}
 }
